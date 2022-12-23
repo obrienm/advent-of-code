fe5974a3ca5468ff6a99b27392686676a2f687bc
Michael O'Brien <obrienm@iastate.edu>
2022-12-23 14:38:22 -0600

    completed day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,11 +2,55 @@ import kotlin.math.*
 import kotlin.collections.*
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        var score = 0
+        for (i in input.indices) {
+            var a = input[i][0]
+            var b = input[i][2]
+            when (a) {
+                'A' -> when (b) {
+                    'X' -> score += 4 //tie
+                    'Y' -> score += 8 //win
+                    'Z' -> score += 3 //loss
+                }
+                'B' -> when (b) {
+                    'X' -> score += 1 //loss
+                    'Y' -> score += 5 //tie
+                    'Z' -> score += 9 //win
+                }
+                'C' -> when (b) {
+                    'X' -> score += 7 //win
+                    'Y' -> score += 2 //loss
+                    'Z' -> score += 6 //tie
+                }
+            }
+        }
+        return score
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var score = 0
+        for (i in input.indices) {
+            var a = input[i][0]
+            var b = input[i][2]
+            when (a) {
+                'A' -> when (b) {
+                    'X' -> score += 3 //loss
+                    'Y' -> score += 4 //tie
+                    'Z' -> score += 8 //win
+                }
+                'B' -> when (b) {
+                    'X' -> score += 1 //loss
+                    'Y' -> score += 5 //tie
+                    'Z' -> score += 9 //win
+                }
+                'C' -> when (b) {
+                    'X' -> score += 2 //loss
+                    'Y' -> score += 6 //tie
+                    'Z' -> score += 7 //win
+                }
+            }
+        }
+        return score
     }
 
     // test if implementation meets criteria from the description, like:
