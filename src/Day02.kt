e23068831e338884930a03e60252266759fd4031
Michael O'Brien <obrienm@iastate.edu>
2022-12-23 13:51:31 -0600

    added day02 files

---
+++ src/Day02.kt
@@ -0,0 +1,19 @@
+import kotlin.math.*
+import kotlin.collections.*
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day02_test")
+//    check(part1(testInput) == 1)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
