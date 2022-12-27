0f95a36fe02257c30a4894639f32d1b5b01395ee
Michael O'Brien <obrienm@iastate.edu>
2022-12-26 21:22:29 -0600

    day06 complete

--- src/Day06.kt
+++ src/Day06.kt
@@ -20,7 +20,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var cnt = 0
+        for (i in 0 until input[0].length-13) {
+            var str = input[0].substring(i, i+14)
+            var set = HashSet<Char>()
+            var charArray = str.toCharArray()
+            set.add(charArray[0])
+            set.add(charArray[1])
+            set.add(charArray[2])
+            set.add(charArray[3])
+            set.add(charArray[4])
+            set.add(charArray[5])
+            set.add(charArray[6])
+            set.add(charArray[7])
+            set.add(charArray[8])
+            set.add(charArray[9])
+            set.add(charArray[10])
+            set.add(charArray[11])
+            set.add(charArray[12])
+            set.add(charArray[13])
+            if (set.size == str.length) break
+            else cnt++
+        }
+        return cnt+14
     }
 
     // test if implementation meets criteria from the description, like:
